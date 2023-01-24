8966b1ead76aff7507225e531b062df264e0f6bb
Igor Danilov <igor.danilov@ihsmarkit.com>
2023-01-24 14:33:18 +0500

    fix: added changes from code review

--- module1/task1-2.js
+++ module1/task1-2.js
@@ -8,4 +8,6 @@ const rStream = fs.createReadStream('./module1/csv/nodejs-hw1-ex1.csv', 'utf-8')
 const wStream = fs.createWriteStream('./output.txt', {encoding: 'utf-8'});
 rStream.on('error', handleError);
 wStream.on('error', handleError);
-rStream.pipe(csv()).pipe(wStream);
+rStream.pipe(csv()).pipe(wStream).on('finish', () => {
+    console.log('[FINISHED EXPORTING CSV TO JSON...]');
+});
